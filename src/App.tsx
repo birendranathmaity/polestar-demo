a08de6ca7bf5bc5736a2c046db2278420a5d42d1
Biren <birendranathm@unitednewdigitalmedia.com>
2025-04-28 01:50:33 +0530

    add

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,18 @@ import Card from "./components/Card";
 import LongDescription from "./components/LongDescription";
 
 const App: React.FC = () => {
- const isMounted = useRef(false);
+  const isMounted = useRef(false);
   const [banners, setBanners] = useState<any>([]);
+  const [contents, setContent] = useState<any>([]);
   const fetchHome = async () => {
     try {
       const data = await graphqlRequest(GET_HOMEPAGE);
       const { sections = [] } = data?.data?.page;
       let items: any = [];
-      sections.map((item: any) => {
-        if (item?.heroTitle)
+      let items1: any = [];
+
+      sections.map((item: any, index: any) => {
+        if (item?.heroTitle && index === 0) {
           items.push({
             title: item.heroTitle,
             imageUrl: item.heroImage.url,
@@ -28,9 +31,18 @@ const App: React.FC = () => {
             color: "#FFF",
             buttons: item?.buttons || [],
           });
+        } else if (item?.heroTitle && index !== 0) {
+          items1.push({
+            title: item.heroTitle,
+            imageUrl: item.heroImage.url,
+            description: item.heroSubtitle,
+            buttons: item?.buttons || [],
+          });
+        }
       });
 
       setBanners(items);
+      setContent(items1);
     } catch (error) {
       console.error(error);
     }
@@ -81,7 +93,26 @@ const App: React.FC = () => {
 
                 <DataTrackArea />
                 <Card />
-                <NextStep />
+                {contents.map(
+                  (
+                    content: {
+                      title: string;
+                      imageUrl: string;
+                      description: string;
+                      buttons: any[];
+                    },
+                    index: React.Key | null | undefined
+                  ) => (
+                    <NextStep
+                      key={index}
+                      title={content.title}
+                      imageUrl={content.imageUrl}
+                      description={content.description}
+                      buttons={content?.buttons}
+                    />
+                  )
+                )}
+
                 <LongDescription />
                 <Footer />
               </div>
